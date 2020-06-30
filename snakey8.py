bf9fc7697fc24b878768be2beaf0f4f85ea9a42d
Will Helsel <will@cyberpayload.com>
2020-06-30 16:58:50 -0500

    added commenting for snakehead attributes

--- snakey8.py
+++ snakey8.py
@@ -6,19 +6,20 @@ import turtle
 
 # Setting up screenplay
 
-window = turtle.Screen()                    #creates application object
-window.title("Snakey8")                     #sets application title name
-window.bgcolor("yellow")                    #sets play area background color
-window.setup(width=800, height=800)         #sets size of play area
+window = turtle.Screen()                    # creates application object
+window.title("Snakey8")                     # sets application title name
+window.bgcolor("yellow")                    # sets play area background color
+window.setup(width=800, height=800)         # sets size of play area
 window.tracer(0)                            # turns off screen updates
 
 # Snakehead
-head = turtle.Turtle()                      #creates the turle object
-head.speed(0)                               #animation speed of the turtle
-head.shape("square")
-head.color("black")
-head.penup()
-head.goto(0,0)
+head = turtle.Turtle()                      # creates the turle object
+head.speed(0)                               # animation speed of the turtle
+head.shape("square")                        # shape of the snake head
+head.color("black")                         # color of the snake head
+head.penup()                                # this allows the snake to NOT draw when moving (pen is up)
+head.goto(0,0)                              # set the snake in the center
+head.direction = "stop"                     # direction of snake head during start of game
 
 
 # Main game loop
