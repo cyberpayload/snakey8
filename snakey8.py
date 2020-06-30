b617bf590c2698db9ebfd29343d2fc75d9c09212
Will Helsel <will@cyberpayload.com>
2020-06-30 16:54:29 -0500

    created screenplay, snakehead and main game loop

--- snakey8.py
+++ snakey8.py
@@ -3,3 +3,26 @@
 # By @cyberpayload
 
 import turtle
+
+# Setting up screenplay
+
+window = turtle.Screen()                    #creates application object
+window.title("Snakey8")                     #sets application title name
+window.bgcolor("yellow")                    #sets play area background color
+window.setup(width=800, height=800)         #sets size of play area
+window.tracer(0)                            # turns off screen updates
+
+# Snakehead
+head = turtle.Turtle()                      #creates the turle object
+head.speed(0)                               #animation speed of the turtle
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+
+
+# Main game loop
+while True:
+    window.update()
+
+window.mainloop()                           #creates main window loop
